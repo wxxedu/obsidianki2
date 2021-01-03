28a66dbf12f767ad83e1a6ceca06b5388dc6208f
wxxedu <wxxedu@126.com>
2021-01-03 09:54:57 +0800

    Corrected Typo

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ vault_name = "Knowledge Base"
 
 path_to_vault = "/Users/xiuxuan/Library/Mobile Documents/iCloud~org~zrey~metion/Documents/Knowledge Base"
 
-whether_to_convert = {"bold": True, "wiki-links": False, "italics": True, "hightlight": False, "inline code": True, "inline math": True, "display math": False}
+whether_to_convert = {"bold": True, "wiki-links": False, "italics": True, "highlight": False, "inline code": True, "inline math": True, "display math": False}
 
 
 
